edd75df25f3a7c4ad1e49b4094f3c5d64d1c8471
starman91 <rwhite.astro@gmail.com>
2015-05-18 13:31:50 -0600

    Updated template with completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## cachematrix.R
+## Matrix storage function. Holds original matrix and inverse of matrix, once computed.
+makeCacheMatrix <- function(matrixStore = matrix()) {
+	inv <- NULL
+	# sets matrix and default NULL of inverse into into global variables (matrixStore and inv)
+	set <- function(y) { 
+		matrixStore <<- y 
+		inv <<- NULL
+	}
+	get <- function() matrixStore  					# Return current stored matrix
+	setInverse <- function(solve){ inv <<- solve }	# Store an inverse of matrix
+	getInverse <- function()inv  					# Return inverse of matrix
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Solves an invertible matrix and caches results. Additional calls to the function 
+## will return previously cached results. NOTE: matrixStore must be previously initialized
+## before calling this function.
+cacheSolve <- function(matrixStore, ...) {
+	# Return a matrix that is the inverse of 'x' 
+	inv <- matrixStore$getInverse()
+	#if results are returned (not NULL), then function will outpu cached results
+	if(!is.null(inv)) {
+		message("getting inverse matrix data")
+		#display stored results
+		return(inv)
+	}
+	# No previous cached results, solve from passed in matrix storage function
+	inv <- solve(matrixStore$get())
+	# Store matrix inverse into matrix storage function
+	matrixStore$setInverse(inv)
+	# Display results
+	inv
 }
+	
+	
